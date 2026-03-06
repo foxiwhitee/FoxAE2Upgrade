3db27986f0f3cb36971335fc6e1de6d689e29850
_WhiteFox <oppovier@gmail.com>
2026-03-06 16:43:11 +0200

    Foxed Card dupe

--- src/main/java/foxiwhitee/FoxAE2Upgrade/block/BlockCobblestoneDuper.java
+++ src/main/java/foxiwhitee/FoxAE2Upgrade/block/BlockCobblestoneDuper.java
@@ -1,12 +1,6 @@
 package foxiwhitee.FoxAE2Upgrade.block;
 
-import appeng.util.Platform;
 import foxiwhitee.FoxAE2Upgrade.tile.TileCobblestoneDuper;
-import net.minecraft.block.Block;
-import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
-
-import java.util.ArrayList;
 
 public class BlockCobblestoneDuper extends BlockApplied {
     public BlockCobblestoneDuper(String name) {
@@ -19,17 +13,4 @@ public class BlockCobblestoneDuper extends BlockApplied {
         return "cobblestoneDuper/";
     }
 
-    public void breakBlock(World world, int x, int y, int z, Block block, int b) {
-        TileCobblestoneDuper te = (TileCobblestoneDuper)world.getTileEntity(x, y, z);
-        if (te != null) {
-            ArrayList<ItemStack> drops = new ArrayList<>();
-            if (te.dropItems()) {
-                te.getDrops(world, x, y, z, drops);
-            } else {
-                te.getNoDrops(world, x, y, z, drops);
-            }
-            Platform.spawnDrops(world, x, y, z, drops);
-        }
-        super.breakBlock(world, x, y, z, block, b);
-    }
 }
